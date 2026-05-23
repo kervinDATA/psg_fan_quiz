5b2afa70453206d60d7b5d6aa155012cdb1e5142
kervindholah <kervin92310@hotmail.fr>
2026-05-23 16:05:08 +0200

    chore: initialize firebase core and connect to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart'; // 👈 L'import magique
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart'; // 👈 L'import du package
+import 'firebase_options.dart'; // 👈 L'import de ta configuration sur-mesure
 import 'app/app.dart';
 
 void main() async {
+  // Obligatoire quand on veut lancer des processus (comme Firebase) avant runApp
   WidgetsFlutterBinding.ensureInitialized();
   
-  // On garde Firebase commenté pour le moment (MVP hors ligne)
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  // 🔴 NOUVEAU : On allume Firebase avec les clés de ton projet
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // On enveloppe l'application dans le ProviderScope pour activer Riverpod
   runApp(
