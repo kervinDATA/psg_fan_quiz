252bb37b23746778eae64c8eb20427d0a25de598
kervindholah <kervin92310@hotmail.fr>
2026-05-30 15:41:33 +0200

    chore: setup firebase crashlytics with web platform guard

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_core/firebase_core.dart'; // 👈 L'import du package
-import 'firebase_options.dart'; // 👈 L'import de ta configuration sur-mesure
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart'; // 👈 Import pour kIsWeb et PlatformDispatcher
+import 'firebase_options.dart';
 import 'app/app.dart';
 
 void main() async {
-  // Obligatoire quand on veut lancer des processus (comme Firebase) avant runApp
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // 🔴 NOUVEAU : On allume Firebase avec les clés de ton projet
+
+  // Initialisation de Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // On enveloppe l'application dans le ProviderScope pour activer Riverpod
+  // 🔴 NOUVEAU : Configuration de Crashlytics (Uniquement pour le mobile)
+  if (!kIsWeb) {
+    // 1. Intercepte toutes les erreurs synchrones (UI) de Flutter
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+    // 2. Intercepte toutes les erreurs asynchrones (réseau, base de données)
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+  }
+
+  // Lancement de l'application
   runApp(
     const ProviderScope(
       child: MyApp(),
